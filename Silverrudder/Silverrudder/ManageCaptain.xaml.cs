0cf3fe22b4ee4ee723b1a4a3f2d9b73e91e271e5
nikol <nikol@DESKTOP-QE1RJ17>
2017-04-27 13:08:31 +0200

    Test

---
+++ Silverrudder/Silverrudder/ManageCaptain.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Silverrudder
+{
+    /// <summary>
+    /// Interaction logic for ManageCaptain.xaml
+    /// </summary>
+    public partial class ManageCaptain : UserControl
+    {
+        public ManageCaptain()
+        {
+            InitializeComponent();
+        }
+    }
+}
