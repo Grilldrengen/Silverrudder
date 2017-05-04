e757beb063d408b86cac23057b2a6e7c2c6cf209
Christian <Christian@CHRISTIAN-HP>
2017-05-04 14:12:06 +0200

    .

---
+++ Silverrudder/DataAccesLayer/LoadXmlFiles.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain;
+using System.Xml;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace DataAccesLayer
+{
+    class LoadXmlFiles
+    {
+        public static List<Participant> LoadParticipantListFromXmlFile()
+        {
+            string participantFile = "participants.xml";
+
+            if (string.IsNullOrEmpty(participantFile)) { return default(List<Participant>); }
+
+            List<Participant> objectOut = default(List<Participant>);
+
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(participantFile);
+            string xmlString = xmlDocument.OuterXml;
+
+            using (StringReader read = new StringReader(xmlString))
+            {
+                Type outType = typeof(List<Participant>);
+
+                XmlSerializer serializer = new XmlSerializer(outType);
+                using (XmlReader reader = new XmlTextReader(read))
+                {
+                    objectOut = (List<Participant>)serializer.Deserialize(reader);
+                    reader.Close();
+                }
+                read.Close();
+            }
+            return objectOut;
+        }
+
+
+        public static List<Participant> LoadCategoryListFromXmlFile()
+        {
+            string categoryFile = "categories.xml";
+
+            if (string.IsNullOrEmpty(categoryFile)) { return default(List<Participant>); }
+
+            List<Participant> objectOut = default(List<Participant>);
+
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(categoryFile);
+            string xmlString = xmlDocument.OuterXml;
+
+            using (StringReader read = new StringReader(xmlString))
+            {
+                Type outType = typeof(List<Participant>);
+
+                XmlSerializer serializer = new XmlSerializer(outType);
+                using (XmlReader reader = new XmlTextReader(read))
+                {
+                    objectOut = (List<Participant>)serializer.Deserialize(reader);
+                    reader.Close();
+                }
+                read.Close();
+            }
+            return objectOut;
+        }
+    }
+}
---
+++ Silverrudder/DataAccesLayer/SaveToXmlFiles.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace DataAccesLayer
+{
+    class SaveToXmlFiles
+    {
+        public static void CreateXMLFiles(string filePath)
+        {
+            if (!File.Exists("participants.xml"))
+            {
+                List<Participant> list = new List<Participant>();
+                XmlDocument xmlDocument = new XmlDocument();
+                XmlSerializer serializer = new XmlSerializer(list.GetType());
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    serializer.Serialize(stream, list);
+                    stream.Position = 0;
+                    xmlDocument.Load(stream);
+                    xmlDocument.Save("participants.xml");
+                    stream.Close();
+                }
+            }
+
+            if (!File.Exists("categories.xml"))
+            {
+                List<Category> list = new List<Category>();
+                XmlDocument xmlDocument = new XmlDocument();
+                XmlSerializer serializer = new XmlSerializer(list.GetType());
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    serializer.Serialize(stream, list);
+                    stream.Position = 0;
+                    xmlDocument.Load(stream);
+                    xmlDocument.Save("categories.xml");
+                    stream.Close();
+                }
+            }
+        }
+
+        public static void SaveParticipantListToXmlFile(List<Participant> list, string file)
+        {
+                if (list == null) { return; }
+
+                XmlDocument xmlDocument = new XmlDocument();
+                XmlSerializer serializer = new XmlSerializer(list.GetType());
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    serializer.Serialize(stream, list);
+                    stream.Position = 0;
+                    xmlDocument.Load(stream);
+                    xmlDocument.Save(file);
+                    stream.Close();
+                }
+        }
+
+        public static void SaveCategoryListToXmlFile(List<Category> list, string file)
+        {
+            if (list == null) { return; }
+
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlSerializer serializer = new XmlSerializer(list.GetType());
+            using (MemoryStream stream = new MemoryStream())
+            {
+                serializer.Serialize(stream, list);
+                stream.Position = 0;
+                xmlDocument.Load(stream);
+                xmlDocument.Save(file);
+                stream.Close();
+            }
+        }
+    }
+}
